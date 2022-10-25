c462d36f998da0421fcb1e7956dc3213f5ae7825
hongyk <120964373@qq.com>
2022-10-25 12:37:00 +0800

    inti

---
+++ cmake-build-debug/CMakeFiles/yuco.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/tmp/tmp.4tKuHn6DON/main.cpp" "/tmp/tmp.4tKuHn6DON/cmake-build-debug/CMakeFiles/yuco.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/yuco.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yuco.dir/main.cpp.o"
+  "yuco"
+  "yuco.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yuco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
